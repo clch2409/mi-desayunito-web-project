4ba9a715c09553b0ae65d69374a1ce3967e2c144
clch2409 <cesarlch2409@gmail.com>
2025-03-25 14:26:41 -0500

    Creando la grilla para una mejor vista de los insumos

--- app/insumos.js
+++ app/insumos.js
@@ -5,13 +5,38 @@ const insumos = insumosData.length > 0 ? insumosData : []
 const insumoForm = document.getElementById('insumo-form');
 
 
-
-
 insumoForm.addEventListener('click', addColorLabel)
+
+mostrarInsumosLabels();
 
 function addColorLabel(event){
   const target = event.target;
-  target.classList.toggle('selected');
+  target.classList.toggle('selected')
+}
 
-  console.log({target})
+function mostrarInsumosLabels(){
+  insumos.forEach(insumo => {
+    crearLabelInputInsumo(insumo.nombre)
+  })
+}
+
+function crearLabelInputInsumo(nombreInsumo){
+  const insumoLabelElement = document.createElement('label');
+  const insumoInputElement = crearInputInsumo(nombreInsumo)
+
+  insumoLabelElement.textContent = nombreInsumo.toUpperCase();
+  insumoLabelElement.classList.add('insumo-name');
+  insumoLabelElement.htmlFor = nombreInsumo;
+
+  insumoLabelElement.insertAdjacentElement('beforeend', insumoInputElement)
+
+  insumoForm.appendChild(insumoLabelElement);
+}
+
+function crearInputInsumo(nombreInsumo){
+  const inputElement = document.createElement('input');
+  inputElement.type = 'checkbox';
+  inputElement.id = nombreInsumo;
+  inputElement.classList.add('insumo-check');
+  return inputElement;
 }
