2acc51601a40e1f3801d640bd8ae7fe35c54abe2
clch2409 <cesarlch2409@gmail.com>
2026-02-05 17:17:12 -0500

    Modificando el objeto con nuevos atributos

--- model/Insumo.mjs
+++ model/Insumo.mjs
@@ -1,7 +1,9 @@
-export default class Insumo{
-  constructor(nombre, proveedor, tipoInsumo){
+export default class Insumo {
+  constructor(nombre, proveedor, tipoInsumo, tipoDeVenta, precio) {
     this.nombre = nombre;
     this.proveedor = proveedor;
     this.tipoInsumo = tipoInsumo;
+    this.tipoDeVenta = tipoDeVenta;
+    this.precio = precio;
   }
 }
